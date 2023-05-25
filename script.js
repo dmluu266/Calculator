22a0959985fb086778fb82be7332c023809feb37
Dong Luu <dluu160@gmail.com>
2023-05-25 03:08:35 -0500

    create functional calculation with display and create button animation

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ let first = '';
 let second = '';
 let op = '';
 
+// display
+const display = document.getElementById('display');
+
 // event listener for number buttons
 const num = document.querySelectorAll('.number');
 
 for (let i=0; i<num.length; i++) {
     num[i].addEventListener('click', () => {
         currNumber += num[i].textContent;
+        display.textContent = currNumber;
         console.log(num[i].textContent);
         console.log(currNumber)
     })
@@ -18,11 +22,18 @@ for (let i=0; i<num.length; i++) {
 
 // event listener for operators
 const operators = document.querySelectorAll('.operator');
-
+let currOperator = '';
 for (let i=0; i<operators.length; i++) {
     operators[i].addEventListener('click', () => {
-        op = operators[i].textContent;
-        first = currNumber;
+        currOperator = operators[i];
+        op = currOperator.textContent;
+        // add operator animation
+        currOperator.classList.add('animation');
+        // set first = currNumber only if first calculation
+        if (first == '') {
+            first = currNumber;
+        }
+        
         // clear currNumber
         currNumber = '';
 
@@ -34,5 +45,22 @@ for (let i=0; i<operators.length; i++) {
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () => {
-    console.log(parseInt(first) + parseInt(currNumber));
+    // + calculation
+    if (op == '+') {
+        first = parseInt(first) + parseInt(currNumber);
+    }
+    else if (op == '-') {
+        first = parseInt(first) - parseInt(currNumber);
+    }
+    else if (op == 'x') {
+        first = parseInt(first) * parseInt(currNumber);
+    }
+    else if (op == '/') {
+        first = parseInt(first) / parseInt(currNumber);
+    }
+    // clear currNumber
+    currNumber = '';
+    // remove operator animation
+    currOperator.classList.remove('animation');
+    display.textContent = first;
 })
